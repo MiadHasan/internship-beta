64fbcd024ff7790eeed34aa756b71d49c44aaa2c
Miad Hasan <mdhasan9.sy@gmail.com>
2024-03-22 12:56:01 +0600

    implemented: update user route

--- Day-4_Node/ExpressMongoCRUD/controllers/user.js
+++ Day-4_Node/ExpressMongoCRUD/controllers/user.js
@@ -9,3 +9,14 @@ export const createUser = async (req, res) => {
     res.status(409).json(err);
   }
 };
+
+export const updateUser = async (req, res) => {
+  const user = req.body;
+  const id = req.params.id;
+  try {
+    const updatedUser = await User.findByIdAndUpdate(id, user);
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(404).json(err);
+  }
+};
--- Day-4_Node/ExpressMongoCRUD/routers/user.js
+++ Day-4_Node/ExpressMongoCRUD/routers/user.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createUser } from "../controllers/user.js";
+import { createUser, updateUser } from "../controllers/user.js";
 
 const router = express.Router();
 
 router.post("/", createUser);
+router.patch("/:id", updateUser);
 
 export default router;
