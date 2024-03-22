66fdab8a4705df1f7f4438a8f9b86c7c72c11d76
Miad Hasan <mdhasan9.sy@gmail.com>
2024-03-22 13:02:31 +0600

    implemented: get user by id route

--- Day-4_Node/ExpressMongoCRUD/controllers/user.js
+++ Day-4_Node/ExpressMongoCRUD/controllers/user.js
@@ -20,3 +20,13 @@ export const updateUser = async (req, res) => {
     res.status(404).json(err);
   }
 };
+
+export const getUserById = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(404).json(err);
+  }
+};
--- Day-4_Node/ExpressMongoCRUD/routers/user.js
+++ Day-4_Node/ExpressMongoCRUD/routers/user.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createUser, updateUser } from "../controllers/user.js";
+import { createUser, getUserById, updateUser } from "../controllers/user.js";
 
 const router = express.Router();
 
 router.post("/", createUser);
 router.patch("/:id", updateUser);
+router.get("/:id", getUserById);
 
 export default router;
